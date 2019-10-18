3344231804a87728f5f5cb6f6c322306f7a040e8
Brandon H <hurlburb@microsoft.com>
2019-10-18 11:33:37 -0700

    updating nuget pkgs

--- AzureFunctions.v2.Durable/DurableContextExtensions.cs
+++ AzureFunctions.v2.Durable/DurableContextExtensions.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.Azure.WebJobs.Host;
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Newtonsoft.Json.Linq;
 
 namespace FileValidation
 {
     static class DurableContextExtensions
     {
-        public static void Log(this Microsoft.Azure.WebJobs.DurableOrchestrationContext context, TraceWriter log, string messsage, bool onlyIfNotReplaying = true)
+        public static void Log(this DurableOrchestrationContext context, TraceWriter log, string messsage, bool onlyIfNotReplaying = true)
         {
             if (!onlyIfNotReplaying || !context.IsReplaying)
             {
@@ -12,6 +14,10 @@ namespace FileValidation
             }
         }
 
-        public static void Log(this Microsoft.Azure.WebJobs.DurableOrchestrationClient client, TraceWriter log, string messsage, bool onlyIfNotReplaying = true) => log.Warning(messsage);
+        public static void Log(this DurableOrchestrationClient client, TraceWriter log, string messsage, bool onlyIfNotReplaying = true) => log.Warning(messsage);
+
+        public static JToken GetInputAsJson(this DurableActivityContextBase ctx) => ctx.GetInput<JToken>();
+
+        public static JToken GetInputAsJson(this DurableOrchestrationContextBase ctx) => ctx.GetInput<JToken>();
     }
 }
